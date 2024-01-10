38a48b36b53547c17d494a2bb65bd8636daa09fa
alesrael <154881297+alesrael@users.noreply.github.com>
2024-01-09 21:56:05 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -1,33 +1,44 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
-st.set_page_config(page_title="Home")
+st.set_page_config(page_title="Home", layout="wide")
 
 # Variaveis
-a = st.sidebar.radio('Menu:',["Pagina 1","Pagina 2"])
-tabela = pd.read_csv("flush.csv", sep=",")
-
-#trecho para xibir caixa de codigo
-st.echo()
-with st.echo():
-    st.write('Code will be executed and printed')
-
-#st.dataframe(tabela)
-
-#st.dataframe(tabela.style.highlight_max(axis=0))
-
-# st.json({f'foo':'bar','fu':'ba'})
-# st.metric(label="Temp", value="273 K", delta="1.2 K")
+#a = st.sidebar.radio('Menu:',["Pagina 1","Pagina 2"])
+tabela = pd.read_csv("Leitoas-2024-01-08.csv", sep=",")
 
 df = pd.DataFrame(tabela)
-df['Brinco'] = df['Brinco'].astype(int)
-selected_raça = st.sidebar.selectbox('Selecione a Raça', df['Raça'].unique())
+
+raca_unica = df['Raça'].unique()
+raca = st.sidebar.multiselect('Selecione a Raça', raca_unica)
+
+df['Ultimo cio'] = pd.to_datetime(df['Ultimo cio'])
+
+# Adicionar filtro de data no Streamlit
+data_inicio = st.sidebar.date_input('Selecione a data de início:', format="DD/MM/YYYY")
+data_fim = st.sidebar.date_input('Selecione a data de fim:', format="DD/MM/YYYY")
+
+# Filtrar o DataFrame com base nas datas selecionadas
+df = df[(df['Ultimo cio'] >= pd.to_datetime(data_inicio)) & (df['Ultimo cio'] <= pd.to_datetime(data_fim))]
 
 # Filtrar o DataFrame com base na raça selecionada
-filtered_df = df[df['Raça'] == selected_raça]
+df = df[df['Raça'].isin(raca)]
+
+idade = st.sidebar.number_input("Idade maior que:", value=225)
+df = df[df['Idade']>= idade]
+
+#reseta o indice da tabela
+df = df.reset_index(drop=True)
 
 # Mostrar o DataFrame filtrado
-st.write(f'DataFrame filtrado para a raça {selected_raça}:')
-st.write(filtered_df, use_column_width=True)
+st.write(f'DataFrame filtrado para a raça {raca}:')
+st.write(df, use_column_width=True)
 
-st.dataframe(df)
+cores_por_raça = {
+                    'TLLZZ - TN70': 'blue',
+                    'TZZZZ - AVÓ':'red'
+}
+
+plotgrafico = px.scatter(tabela, x="Idade", y="TSI", color="Raça", color_discrete_map=cores_por_raça)
+st.plotly_chart(plotgrafico, use_container_width=True)
